cd504c392246d4e77436d3495761d97725c03eb1
HR77scu <rafiasik77@gmail.com>
2024-02-23 14:31:53 +0700

    feat(pemasukan): remove comment in controller

--- app/Http/Controllers/PemasukanController.php
+++ app/Http/Controllers/PemasukanController.php
@@ -7,18 +7,18 @@ use Illuminate\Http\Request;
 class PemasukanController extends Controller
 {
     public function index(){
-        // 
+        
     }
     public function create(){
-        // 
+        
     }
     public function store(Request $request){
-        // 
+        
     }
     public function edit($id){
-        // 
+        
     }
     public function update(Request $request, $id){
-        // 
+        
     }
 }
